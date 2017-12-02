dc21796e8ef7a2f633a8759cdbcbf8501220977a
Unknown <IwraStudios@hotmail.com>
2017-12-02 23:45:40 +0100

    Make it a Promise

--- README.md
+++ README.md
@@ -1,17 +1,47 @@
 # node-cachefile
 A very small node module for caching &amp; syncing files to ram with virtually zero downtime
 
+Recommended with small-file/web servers
+
 Usage:
 ```js
 
 var cache = require("cachefile");
 
-cache.AddFileToCache("abcd", "./README.md");
-console.log(cache.cachedFiles["abcd"]);
+cache.AddFileToCache("abcd", "./README.md").then(() =>{
+    console.log(cache.cachedFiles["abcd"]);
+});
+
+//Do some stuff that takes some time
+
 //Update the file
 cache.SyncCache();
 //Changed output
 console.log(cache.cachedFiles["abcd"]);
 ```
 
-TODO: make it a promise ;)
+WebServer implication:
+```js
+var cache = require("cachefile");
+var async = require("async");
+
+cache.AddFileToCache("abcd", "./README.md");
+
+function UpdateRoutine() {
+    async.parallel([
+        cache.SyncCache,
+        SomeOtherFunction
+    ]);
+    setTimeout(UpdateRoutine, 60000);//You should use a diffrent function
+    console.log("Did periodic routine: ", new Date().toString("yyyyMMddHHmmss"));
+}
+
+//cached pages auto update
+setTimeout( UpdateRoutine,5000);
+
+///Serve a file like so
+        response.write(cache.cachedFiles["abcd"], "binary");
+///The webserver will update the file every 60 seconds
+///Without every haveing the file unavailable
+
+```
--- cache.js
+++ cache.js
@@ -1,32 +1,40 @@
 var cachedFiles = {};
 //Every key has (key: {filecontent: data, filepath: /x/amp.le, timestamp: date.then})
- 
+
 module.exports = {
     //Example FS.AddFileToCache('welcome', '/README.md');
     AddFileToCache: function (name, filerpath) {
-        var fs = require('fs');
-        fs.readFile(__dirname + filerpath, 'utf8', function (err, data) {
-            if (err) {
-                console.log(err);
-            }
-            cachedFiles[name] = new Object;
-            cachedFiles[name]['content'] = data;
-            cachedFiles[name]['path'] = filerpath;
-            cachedFiles[name]['timestamp'] = Date.now();
+        return new Promise(function (resolve, reject) {
+            var fs = require('fs');
+            fs.readFile(__dirname + filerpath, 'utf8', function (err, data) {
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                }
+                cachedFiles[name] = new Object;
+                cachedFiles[name]['content'] = data;
+                cachedFiles[name]['path'] = filerpath;
+                cachedFiles[name]['timestamp'] = Date.now();
+            });
+            resolve();
         });
     },
 
     SyncCache: function () {
-        var fs = require('fs');
-        for (file in cachedFiles) {
-            fs.readFile(__dirname + cachedFiles[file]['path'], 'utf8', function (err, data) {
-                if (err) {
-                    console.log(err);
-                }
-                cachedFiles[file]['content'] = data;
-                cachedFiles[file]['timestamp'] = Date.now();
-            });
-        }
+        return new Promise(function (resolve, reject) {
+            var fs = require('fs');
+            for (file in cachedFiles) {
+                fs.readFile(__dirname + cachedFiles[file]['path'], 'utf8', function (err, data) {
+                    if (err) {
+                        console.log(err);
+                        reject(err);
+                    }
+                    cachedFiles[file]['content'] = data;
+                    cachedFiles[file]['timestamp'] = Date.now();
+                });
+            }
+            resolve();
+        });
     },
 
     cachedFiles: cachedFiles
