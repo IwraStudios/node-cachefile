a8cb5af78c760bb8ca1e12c547ef4de4f2426e19
Unknown <IwraStudios@hotmail.com>
2017-12-03 11:04:30 +0100

    Better Promises

--- README.md
+++ README.md
@@ -5,6 +5,34 @@ Recommended with small-file/web servers
 
 `var cache = require("cachefile");` can also be `var cache = require("./cache");` depending on the way you import it
 
+`SyncCache` Will give a promise for every file, so you can keel track of what is going on with something like:
+
+```js
+
+Promise.all(cache.SyncCache()).then(values => { 
+  console.log(values);//All were loaded correctly
+}).catch(reason => { 
+  console.log(reason)l //Some coulnd't be loaded
+});
+
+
+```
+
+Or with more control:
+
+```js
+var promises = cache.SyncCache();
+for(promi in promises){
+    promi.then(path =>{
+        console.log("updated: " + path)
+    }).catch( err =>{
+        console.log("Failed to update file: ", err)
+    });
+}
+
+```
+
+
 npm not yet available
 
 You can see the last synctime(Unix epoch) like this:
@@ -27,9 +55,10 @@ cache.AddFileToCache("abcd", "./README.md").then(() =>{
 //Do some stuff that takes some time
 
 //Update the file
-cache.SyncCache();
-//Changed output
-console.log(cache.cachedFiles["abcd"]);
+Promise.all(cache.SyncCache()).then(() =>{
+    //Changed output
+    console.log(cache.cachedFiles["abcd"]);
+});
 ```
 
 WebServer implication:
--- cache.js
+++ cache.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var cachedFiles = {};
 //Every key has (key: {filecontent: data, filepath: /x/amp.le, timestamp: date.then})
 
@@ -5,7 +6,6 @@ module.exports = {
     //Example FS.AddFileToCache('welcome', '/README.md');
     AddFileToCache: function (name, filerpath) {
         return new Promise(function (resolve, reject) {
-            var fs = require('fs');
             fs.readFile(__dirname + filerpath, 'utf8', function (err, data) {
                 if (err) {
                     console.log(err);
@@ -15,25 +15,24 @@ module.exports = {
                 cachedFiles[name]['content'] = data;
                 cachedFiles[name]['path'] = filerpath;
                 cachedFiles[name]['timestamp'] = Date.now();
+                resolve();
             });
-            resolve();
         });
     },
 
     SyncCache: function () {
-        return new Promise(function (resolve, reject) {
-            var fs = require('fs');
-            for (file in cachedFiles) {
+        return cachedFiles.map(file => {
+            return new Promise(function (resolve, reject) {
                 fs.readFile(__dirname + cachedFiles[file]['path'], 'utf8', function (err, data) {
                     if (err) {
                         console.log(err);
-                        reject(err);
+                        return reject(err);
                     }
                     cachedFiles[file]['content'] = data;
                     cachedFiles[file]['timestamp'] = Date.now();
+                    resolve(cachedFiles[file]['filepath']);
                 });
-            }
-            resolve();
+            });
         });
     },
 
